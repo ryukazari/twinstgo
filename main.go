c81ae622e49055be63c2dc9a41f8a9d8298647e1
rogger <rogger.140998@gmail.com>
2020-07-28 17:44:08 -0500

    main.go server running

--- main.go
+++ main.go
@@ -1,5 +1,16 @@
 package main
 
-func main() {
+import (
+	"log"
 
+	"github.com/ryukazari/twinstgo/database"
+	"github.com/ryukazari/twinstgo/handlers"
+)
+
+func main() {
+	if database.CheckConnection() {
+		log.Fatal("Failed to connect to the database")
+		return
+	}
+	handlers.Handlers()
 }
